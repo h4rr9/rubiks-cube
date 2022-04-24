c84858cbe02b683c6e272c81b3ee9b383d89b9c4
Hariganesh Srinivasan <hari.ganesh.friendship@gmail.com>
2022-04-24 18:40:39 -0400

    feat(metric): imp Display for MetricKind

--- benches/turn_bench.rs
+++ benches/turn_bench.rs
@@ -1,10 +1,10 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use rubikscube::{Cube, MetricKind, Turn};
+use rubikscube::{Cube, MetricKind};
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut cube = Cube::new(MetricKind::HalfTurnMetric);
-    c.bench_function("cube turn", |b| b.iter(|| cube._turn(black_box(Turn::F))));
+    c.bench_function("cube turn", |b| b.iter(|| cube.turn(black_box(0))));
 }
 
 criterion_group!(benches, criterion_benchmark);
